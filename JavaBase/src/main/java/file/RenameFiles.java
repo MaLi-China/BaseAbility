418610c0fe1b773c8e248eb6b5befd9b13eec77d
MaLi-China <malijava@163.com>
2021-08-19 18:20:40 +0800

    工具类: 批量重命名文件夹内文件

---
+++ JavaBase/src/main/java/file/RenameFiles.java
@@ -0,0 +1,21 @@
+package file;
+
+import java.io.File;
+
+/**
+ * 功能说明: 修改文件夹内所有文件的名称为原名称去掉广告
+ * 开发人员：@Author MaLi
+ */
+public class RenameFiles {
+    public static void renameFiles(String folderPath) {
+        File[] files = new File(folderPath).listFiles();
+        for (File file : files) {
+            String parentPath = file.getParentFile().getPath();
+            String docName = file.getName();
+            int index = docName.indexOf("-");
+            String substring = "\\" + docName.substring(0, index) + ".mp4";
+            String newName = parentPath + substring;
+            file.renameTo(new File(newName));
+        }
+    }
+}
