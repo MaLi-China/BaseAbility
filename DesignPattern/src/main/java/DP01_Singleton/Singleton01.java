341372fd5abd5160427402e7eb1bbc64367805a6
MaLi-China <malijava@163.com>
2021-09-06 00:02:16 +0800

    设计模式: 单例-饿汉式

--- DesignPattern/src/main/java/DP01_Singleton/Singleton01.java
+++ DesignPattern/src/main/java/DP01_Singleton/Singleton01.java
@@ -1,8 +1,23 @@
 package DP01_Singleton;
 
 /**
- * 功能说明：
+ * 功能说明：饿汉式
+ * 1, 私有化构造函数;
+ * 2, 对外提供获取该对象的方法;
+ * <p>
  * 开发人员：@author MaLi
  */
 public class Singleton01 {
+    private static final Singleton01 instance = new Singleton01();
+
+    /*static {
+        instance = new Singleton01();
+    }*/
+
+    private Singleton01() {
+    }
+
+    public static Singleton01 getInstance() {
+        return instance;
+    }
 }
