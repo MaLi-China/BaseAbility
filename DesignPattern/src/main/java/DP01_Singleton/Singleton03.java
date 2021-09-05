b5562279b5a68b7ad9ef1885cb205f86d95b628e
MaLi-China <malijava@163.com>
2021-09-06 00:11:39 +0800

    设计模式: 单例-内部类方式创建单例

--- DesignPattern/src/main/java/DP01_Singleton/Singleton03.java
+++ DesignPattern/src/main/java/DP01_Singleton/Singleton03.java
@@ -2,7 +2,7 @@ package DP01_Singleton;
 
 /**
  * 功能说明：使用内部类的方式, JVM保证Class只被加载一次, 同时保证线程安全
- * 内部类只有在使用的时候才会被JVM加载.
+ * 内部类只有在使用的时候才会被JVM加载.且只被加载一次
  * 开发人员：@author MaLi
  */
 public class Singleton03 {
