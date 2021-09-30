f9f2d461c2261bba47e5127497bf2292efb8d263
MaLi-China <malijava@163.com>
2021-09-30 15:36:43 +0800

    工具使用

--- JavaBase/src/main/java/file/RenameFiles.java
+++ JavaBase/src/main/java/file/RenameFiles.java
@@ -7,18 +7,25 @@ import java.io.File;
  * 开发人员：@Author MaLi
  */
 public class RenameFiles {
-    public static void renameFiles(String folderPath) {
+    /**
+     * 去除文件夹下所有文件的名称广告
+     *
+     * @param folderPath 文件夹
+     * @param postfix    文件后缀
+     */
+    public static void renameFiles(String folderPath, String postfix) {
         File[] files = new File(folderPath).listFiles();
         for (File file : files) {
             String parentPath = file.getParentFile().getPath();
             String docName = file.getName();
             int index = docName.indexOf("-");
-            String substring = "\\" + docName.substring(0, index) + ".mp4";
+            String substring = "\\" + docName.substring(0, index) + postfix;
             String newName = parentPath + substring;
             file.renameTo(new File(newName));
         }
     }
 
     public static void main(String[] args) {
+        renameFiles("E:\\01.架构师课程\\03.拉勾高级架构师\\01 第一阶段 开源框架源码剖析\\模块1 持久层框架设计实现及MyBatis源码分析\\任务3：MyBatis源码剖析", ".mp4");
     }
 }
