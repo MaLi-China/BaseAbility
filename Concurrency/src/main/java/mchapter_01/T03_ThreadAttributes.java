f968143813a026da65f2e07ec00f734708bbc005
MaLi-China <malijava@163.com>
2021-10-04 22:39:38 +0800

    多线程基础

--- Concurrency/src/main/java/mchapter_01/T03_ThreadAttributes.java
+++ Concurrency/src/main/java/mchapter_01/T03_ThreadAttributes.java
@@ -6,6 +6,8 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 功能说明：
+ * 1, 线程的属性
+ * 2, 线程的状态: join之后, 等待线程退出后, 再去查看它的状态: TERMINATED
  * 开发人员：@author MaLi
  */
 public class T03_ThreadAttributes {
@@ -20,13 +22,19 @@ public class T03_ThreadAttributes {
             System.out.println("ThreadName: " + threadName + "\r\nThreadGroup: " + threadGroup + "\r\nThreadID: " + id + "\r\nThreadPriority: " + priority);
             System.out.println("ThreadState: " + thread.getState());
             try {
-                TimeUnit.SECONDS.sleep(10);
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
+        System.out.println("T1.state: " + t1.getState());
         t1.setDaemon(false);//This method must be invoked before the thread is started.
         t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
