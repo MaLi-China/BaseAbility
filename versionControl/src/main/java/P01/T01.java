d1ae6c7a1090eb46aef96d0cddfd7d3832a739df
MaLi-China <malijava@163.com>
2021-10-13 11:36:31 +0800

    测试Maven Version

---
+++ versionControl/src/main/java/P01/T01.java
@@ -0,0 +1,11 @@
+package P01;
+
+/**
+ * 功能说明：
+ * 开发人员：@author MaLi
+ */
+public class T01 {
+    public static void main(String[] args) {
+        System.out.println("Test version of JDK...");
+    }
+}
