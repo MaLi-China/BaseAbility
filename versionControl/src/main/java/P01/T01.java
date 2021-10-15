61eb99db71688fc4fa9b6bd99b2b98350f232b9f
MaLi-China <malijava@163.com>
2021-10-15 10:41:10 +0800

    测试JDK版本/Idea参数调优

--- versionControl/src/main/java/P01/T01.java
+++ versionControl/src/main/java/P01/T01.java
@@ -7,5 +7,18 @@ package P01;
 public class T01 {
     public static void main(String[] args) {
         System.out.println("Test version of JDK...");
+        char[] chars = {'1', '2'};
+        String.copyValueOf(chars, 0, 1);
+        String s = "123";
+    }
+
+    /**
+     * 解释说明方法
+     *
+     * @param msg 消息参数
+     */
+    public static void testMethods1(String msg, String telephone) {
+
+
     }
 }
