b2b0dff51a6d59b7d3ce541b150aa0cb0d26e03c
MaLi-China <malijava@163.com>
2021-11-01 17:05:12 +0800

    字节码格式探索

---
+++ JVM/src/main/java/Chapter11_ByteCode/T01_ByteCode.java
@@ -0,0 +1,11 @@
+package Chapter11_ByteCode;
+
+/**
+ * 功能说明：用于查看字节码文件ain
+ * 开发人员：@author MaLi
+ */
+public class T01_ByteCode {
+    public static void main(String[] args) {
+        System.out.println("Hello JVM");
+    }
+}
