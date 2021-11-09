13e2635db63fc3fd31044a01a4476cc7debd241d
MaLi-China <malijava@163.com>
2021-11-09 15:11:31 +0800

    测试cpu读取主存的缓存行对伪共享的影响

---
+++ JVM/src/main/java/Chapter13_JMM/T03_CacheLine.java
@@ -0,0 +1,53 @@
+package Chapter13_JMM;
+
+/**
+ * 功能说明：测试缓存行
+ * CPU读取一次缓存行空间为64byte
+ * 开发人员：@author MaLi
+ */
+public class T03_CacheLine {
+    //设计一个用作占位符的类
+    private static class T extends Padding {
+        private volatile long placeholder = 0L;
+
+    }
+
+    private static class Padding {
+        public volatile long p1, p2, p3, p4, p5, p6, p7;
+    }
+
+    //创建一个数组, 占位空间小于64byte(目的: 数组内的两个元素同时落入同一个64byte的缓存行内)
+    public static T[] arr = new T[2];
+    public static int _100M = 1000_0000;
+
+    static {
+        arr[0] = new T();
+        arr[1] = new T();
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < _100M; i++) {
+                arr[0].placeholder = i;
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < _100M; i++) {
+                arr[1].placeholder = i;
+            }
+        });
+
+        try {
+            long start = System.nanoTime();
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            long end = System.nanoTime();
+            System.out.println("Time Wasted: " + (end - start));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
